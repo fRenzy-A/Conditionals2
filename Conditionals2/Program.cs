115055ebd9c4b1d81a72d32aa33a92b2917a9273
fRenzy <fRenzyA05@gmail.com>
2022-10-12 15:36:08 -0300

    Lots of changes

--- Conditionals2/Program.cs
+++ Conditionals2/Program.cs
@@ -8,8 +8,75 @@ namespace Conditionals2
 {
     internal class Program
     {
+        static int health = 0;
+        
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int switchWeapon = Convert.ToInt32(Console.ReadLine());
+                
+                ShowHUD(switchWeapon);
+
+            }
+
+           
+            
+
+            Console.WriteLine("Conditionals 2");
+            Console.WriteLine();
+
+            
+            //TakeDamage(150);
+            Heal(10);
+            Console.WriteLine("Health : " + health);
+            
         }
+        // THERES AN '&&' FOR 'AND' LOGIC LETS GOOOOO
+        static void Heal(int heal)
+        {
+            Console.WriteLine("Player healed +" + heal);
+            health = health + heal;
+            if (health >= 100)
+            {
+                Console.WriteLine("Health full");
+                health = 100;
+            }
+        }
+        static void TakeDamage(int damage)
+        {
+            Console.WriteLine("Player takes " + damage + " damage");
+            health = health - damage;
+
+            if (health <= 0)
+            {
+                health = 0;
+            }
+        }
+
+        static void ShowHUD(int weapon)
+        {
+            string gun;
+            if (weapon == 1)
+            {
+                gun = "Pistol";
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Weapon: " + gun);
+            }
+            else if (weapon == 2)
+            {
+                gun = "Rifle";
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Weapon: " + gun);
+            }
+            else
+            {
+                gun = "Unequipped";
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Weapon: " + gun);
+            }
+        }
+        
+        
     }
 }
